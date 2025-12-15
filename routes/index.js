956e0dc85298c31004a72f2e0842ca94fa6b4f9e
Jonah Elliott <jonahelliott@u.boisestate.edu>
2025-12-15 05:03:58 +0000

    Added ability to edit reviews, added date created feature, and cleaned up code comments

--- routes/index.js
+++ routes/index.js
@@ -65,10 +65,14 @@ router.get('/reviews', function(req, res, next){
   }
 });
 
+/* Add new review */
 router.post('/create', function (req, res, next) {
     const { review, review_body } = req.body;
+    const date = new Date().toLocaleString('en-US', {
+        timeZone: 'America/Boise'
+    });
     try {
-      req.db.query('INSERT INTO reviews (review, review_body) VALUES (?, ?);', [review, review_body], (err, results) => {
+      req.db.query('INSERT INTO reviews (review, review_body, date) VALUES (?, ?, ?);', [review, review_body, date], (err, results) => {
         if (err) {
           console.error('Error adding Review:', err);
           return res.status(500).send('Error adding Review');
@@ -83,6 +87,7 @@ router.post('/create', function (req, res, next) {
     }
 });
 
+/* Delete review */
 router.post('/delete', function (req, res, next) {
     const { id } = req.body;
     try {
@@ -98,6 +103,44 @@ router.post('/delete', function (req, res, next) {
     }catch (error) {
         console.error('Error deleting Review:', error);
         res.status(500).send('Error deleting Review:');
+    }
+});
+
+/* Change the review to edit mode */
+router.post('/edit', function (req, res, next) {
+    const { id } = req.body;
+    try {
+      req.db.query('SELECT * FROM reviews;', (err, results) => {
+        if (err) {
+            console.error('Error editing Review:', err);
+            return res.status(500).send('Error editing Review');
+        }
+        res.render('reviews', {
+            reviews: results,
+            editId: id
+        });
+        console.log('Review changed to edit mode successfully:', results);
+    });
+    }catch (error) {
+        console.error('Error editing Review:', error);
+        res.status(500).send('Error editing Review:');
+    }
+});
+
+/* Update review after editing */
+router.post('/update', function (req, res, next) {
+    const {id, review, review_body} = req.body;
+    try {
+        req.db.query('UPDATE reviews SET review = ?, review_body = ? WHERE id = ?', [review, review_body, id], (err, results) => {
+            if (err) {
+                console.error('Error updating Review:', err);
+                return res.status(500).send('Error editing Review');
+            }
+            console.log('Review updated successfully:', results);
+            res.redirect('/reviews');
+        });
+    } catch (error) {
+
     }
 });
 
